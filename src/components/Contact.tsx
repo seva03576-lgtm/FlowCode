9e11a640e0fd1034da9002ffb277578368e6a1f9
Martin <seva03576@gmail.com>
2026-06-23 17:07:51 +0500

    Update Contact.tsx

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -183,7 +183,7 @@ const Contact = () => {
             initial={{ opacity: 0, x: 32 }}
             animate={inView ? { opacity: 1, x: 0 } : {}}
             transition={{ duration: 0.55, delay: 0.15 }}
-            className="lg:mt-[110px]"
+            className="lg:mt-[115px]"
 >
             <form
               ref={formRef}
